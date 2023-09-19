1ad66b15ba377c82adbec69e44fc02b398afe80e
Jean Dresbach <jeanr.dresbach@gmail.com>
2023-09-19 14:58:03 -0300

    refactored project

--- scripts.js
+++ scripts.js
@@ -1,71 +1,96 @@
-const api = axios.create({
-    baseURL: "https://rickandmortyapi.com/api"
-});
+const api = axios.create();
 
-const containerCards = document.getElementById("container-cards");
-const prevPage = document.getElementById("prevPage");
-const nextPage = document.getElementById("nextPage");
+const containerCards = document.getElementById("container-cards")
+const prevPage = document.getElementById("prevPage")
+const nextPage = document.getElementById("nextPage")
 
-async function fetchCharacters() {
+let characters
+
+async function fetchCharacters(url) {
     try {
-        const response = await api.get("/character");
+        const URL = url ?? "https://rickandmortyapi.com/api/character"
         
-        const characters = response.data.results;
-        const info = response.data.info;
+        const response = await api.get(URL)
 
-        
-        console.log(response.data);
-        showCharacters(characters);
+        characters = response.data.results
+        const info = response.data.info
+
+        console.log(response.data)
+        showCharacters(characters)
     } catch (error) {
-        console.log(`Error when fetching characters: ${error}`);
+        console.log(`Error when fetching characters: ${error}`)
     }
 }
 
-function showCharacters(characters) {   
-    characters.forEach(character => {
-        const card = document.createElement("div");
-        card.classList.add('card');
-        card.style.backgroundColor = "none";
-        card.style.border = "none";
-        card.style.padding = "0";
-        card.innerHTML = `
-            <img src="${character.image}">
-        `;
-        card.addEventListener("click", () => {
-            let statusColor = "";
-            if(character.status === "Alive") {
-                statusColor = "#B6F300";
-            } else if(character.status === "Dead") {
-                statusColor = "#E23838";
-            } else {
-                statusColor = "#737373";
-            }
-            
-            containerCards.innerHTML = `
-                <img id="close-button" src="./images/remove.png" alt="close-icon"> 
-            `;
-            
-            const closeButton = document.getElementById("close-button");
-            closeButton.style.display = "block";
-            closeButton.addEventListener("click", () => {
-                window.location.reload();
-            });
-            card.style.backgroundColor = "#00b4cc90";
-            card.style.border = "2px solid white";
-            card.style.padding = "30px";
-            card.innerHTML = `
-                <img id="focused-card" style="opacity: 1; filter: grayscale(0); transform: scale(1)" src="${character.image}">
-                <h2>${character.name}</h2>
-                <p>${character.species} - ${character.gender} - <span style="font-weight: bold; color:${statusColor}">${character.status}</span></p>
-                <p>Origin: ${character.origin.name}</p>
-                <p>Last known location: ${character.location.name}</p>
-            `;
-            
-            containerCards.appendChild(card);
-        })
+function showCharacters(characters) {
+    containerCards.innerHTML = ""
 
-        containerCards.appendChild(card);
+    characters.forEach(character => {
+        createCard(character)
     });
 }
 
-fetchCharacters();
+function createCard(character) {
+    const characterImg = document.createElement("img")
+    characterImg.src = character.image
+    characterImg.classList.add("character-img")
+
+    containerCards.appendChild(characterImg)
+
+    characterImg.addEventListener("click", (e) => {
+        e.preventDefault()
+        showDetailedCharacterCard(character)
+    })
+}
+
+function createDetailedCharacterCard(character) {
+    const statusColor = defineStatusColor(character)
+    
+    const detailedCharacterCard = document.createElement("div")
+    detailedCharacterCard.classList.add("detailed-character-card")
+
+    detailedCharacterCard.innerHTML = `
+        <img id="detailed-character-image" src="${character.image}">
+        <div>
+            <h2 class='text-h2'>${character.name}</h2>
+            <p>${character.species} - ${character.gender} - <span style="color:${statusColor}">${character.status}</span></p>
+            <p>Origin: ${character.origin.name}</p>
+            <p>Last known location: ${character.location.name}</p>
+        </div>
+    `
+
+    containerCards.appendChild(detailedCharacterCard)
+
+    const detailedCharacterImage = document.getElementById("detailed-character-image")
+
+    detailedCharacterImage.addEventListener("click", (e) => {
+        e.preventDefault()
+        closeDetailedCharacterCard(characters)
+    })
+
+    return detailedCharacterCard
+}
+
+function closeDetailedCharacterCard(characters) {
+    showCharacters(characters)
+}
+
+function showDetailedCharacterCard(character) {
+    containerCards.innerHTML = ""
+    createDetailedCharacterCard(character)
+}
+
+function defineStatusColor(character) {
+    let statusColor = ""
+    if (character.status === "Alive") {
+        statusColor = "#B6F300"
+    } else if (character.status === "Dead") {
+        statusColor = "#E23838"
+    } else {
+        statusColor = "#b3b3b3"
+    }
+
+    return statusColor
+}
+
+fetchCharacters()
